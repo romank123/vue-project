33f8bedc24a225e50501ab4a25968a916c3d7e5f
romank123 <67678390+romank123@users.noreply.github.com>
2021-11-15 16:07:52 +0300

    fixed

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:napastyuk/sea-capital-wizard.git master:gh-pages
+git push -f git@github.com:romank123/sea-capital-wizard.git master:gh-pages
 
 cd -
